3ccd65f5fac54f873a4b4731ad0f3b0f73ac073b
Chuanromanee Tya <chua0563@KU.KETTERING.EDU>
2017-06-28 16:15:44 -0400

    Learn about loops

--- Skillsoft/conditions.r
+++ Skillsoft/conditions.r
@@ -20,3 +20,25 @@ n <- c(6,5,4)
 (m < 2) & (n > 5)
 (m < 2) && (n > 5)
 # & is vector friendly, && is for scalars only
+
+
+# Loops!
+# Syntax: for(variable in vector)
+for(a in m) {
+  print(a)
+}
+
+# Repeat loop loops forever, need to break to exit,
+# almost like a do while
+repeat {
+  b <- b + 1
+  if (b == 10) {
+    break;
+  }
+}
+b
+
+# while loop
+while (b > 0) {
+  b < b - 1
+}
