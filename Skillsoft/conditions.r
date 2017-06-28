7c36c96c6c12f07452f02e3049343634e8cee359
Chuanromanee Tya <chua0563@KU.KETTERING.EDU>
2017-06-28 15:53:13 -0400

    learning about control structures

---
+++ Skillsoft/conditions.r
@@ -0,0 +1,14 @@
+# Control statements
+a <- 1
+if (a < 1) {
+  print("a < 1")
+} else if (a > 1) {
+  print ("a > 1")
+} else {
+  print("boooooo")
+}
+
+b <- 2
+if (a == 0 || b == 2) {
+  print ("One has to be the winner")
+}
