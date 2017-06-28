d208bc18138931e3c670299339c2f2e61b9efd37
Chuanromanee Tya <chua0563@KU.KETTERING.EDU>
2017-06-28 16:12:47 -0400

    Add single & condition

--- Skillsoft/conditions.r
+++ Skillsoft/conditions.r
@@ -12,3 +12,11 @@ b <- 2
 if (a == 0 || b == 2) {
   print ("One has to be the winner")
 }
+
+m <- c(1,2,3)
+n <- c(6,5,4)
+# & vectorized, 
+# && first element only
+(m < 2) & (n > 5)
+
+# & is vector friendly, && is for scalars only
