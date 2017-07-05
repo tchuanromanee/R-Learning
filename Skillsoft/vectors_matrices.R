4d83aaf325050e2f683619a61c7c3dcaed0c96f3
tchuanromanee <chua0563@kettering.edu>
2017-07-05 14:47:02 -0400

    Vector operations, basic

--- Skillsoft/vectors_matrices.R
+++ Skillsoft/vectors_matrices.R
@@ -7,3 +7,14 @@ y <- 2
 
 # Create a new vector from existing
 z <- c(y,y * 2, y + 1, 0, v)
+z
+
+# Operations on vectors
+a <- v * y
+a
+
+# different size vectors
+a <- c(1,2)
+b <- c(1,2,3,4)
+d <- a + b # Works is b is a multiple of a
+d
