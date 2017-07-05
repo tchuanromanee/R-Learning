6dea56646600d62cfd5f5b19e2d301b8887e21f5
tchuanromanee <chua0563@kettering.edu>
2017-07-05 14:49:47 -0400

    Vector functions for operations

--- Skillsoft/vectors_matrices.R
+++ Skillsoft/vectors_matrices.R
@@ -10,7 +10,7 @@ z <- c(y,y * 2, y + 1, 0, v)
 z
 
 # Operations on vectors
-a <- v * y
+a <- v * y # Multiply a vector by a scalar
 a
 
 # different size vectors
@@ -18,3 +18,16 @@ a <- c(1,2)
 b <- c(1,2,3,4)
 d <- a + b # Works is b is a multiple of a
 d
+
+# math functions operate on the entire vector
+sqrt(d)
+
+# seq and rep
+v <- 1:10 # Numbers from 1 to 10
+v
+
+v <- seq(5,100,by=5) # From 5 to 100 increment by 5
+v
+
+v <- rep(1:5,5) # 1 2 3 4 5 repeated 5 times
+v
